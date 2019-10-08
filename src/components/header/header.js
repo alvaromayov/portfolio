8eef96111cc675c4e152e033f42af283212d2aee
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-08 13:02:23 +0100

    Add color header color change on scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Header() {
-    return (
-        <nav className="header">
-            <div className="header__logo">Icon</div>
-            <div className="header__links">
-                <a className="header__link" href="/">Home</a>
-                <a className="header__link" href="/">Blog</a>
-                <a className="header__link" href="/">Projects</a>
-                <a className="header__link" href="/">Contact</a>
-            </div>
-        </nav>
-    )
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            positionY: null,
+            isTop: "header--top"
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('scroll', () => {
+          const isTop = window.scrollY;
+          if (isTop > 100) {
+              this.setState({
+                  isTop: "header--not-top"
+              })
+            } else {
+                this.setState({
+                    isTop: "header--top"
+                })
+            }
+            console.log(this.state.isTop)
+        });
+      }
+
+    render() {
+        return(
+            <nav className={"header " + this.state.isTop}>
+                <div className="header__logo">Icon</div>
+                <div className="header__links">
+                    <a className="header__link" href="/">Home</a>
+                    <a className="header__link" href="/">Blog</a>
+                    <a className="header__link" href="/">Projects</a>
+                    <a className="header__link" href="/">Contact</a>
+                    <button onClick={this.scroll}>click</button>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Header
