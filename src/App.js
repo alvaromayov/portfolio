89d293ff40e68d1bca8b85c744f8da66704b1161
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-14 15:56:50 +0100

    Add unveil animation on arrival

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
+import Unveil from './components/unveil/Unveil';
 
 function App() {
   return (
     <div className="App">
-     <Header />
+      <Unveil />  
+      <Header />
     </div>
   );
 }
