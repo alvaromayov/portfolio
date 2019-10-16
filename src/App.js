715025a96942d1aa61576379394bbe0edee86a5e
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-16 15:20:51 +0100

    Change body

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Hero 
           heading="Don't just build a website"
           title="Build digital experiences"
-          body="Hello! My name is Alvaro and I am a Front-End Developer. Play around with my site to know a bit more about me and what I do."
+          body="Hello! My name is Alvaro and I am a Front-End Developer. Play around with my site to learn a bit more about me and what I do."
         />
       </main>
     </div>
