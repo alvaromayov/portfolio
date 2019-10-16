19dbba9779bfbc09efac9b6d346a73d0d37241ec
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-16 14:04:15 +0100

    Add copy to hero

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ function App() {
     <div className="App">
       <Unveil />  
       <Header />
-      <Hero 
-        heading="Don't just build a website"
-        title="Build digital experiences"
-      />
+      <main>
+        <Hero 
+          heading="Don't just build a website"
+          title="Build digital experiences"
+          body="Hello! My name is Alvaro and I am a Front-End Developer. Play around with my site to know a bit more about me and what I do."
+        />
+      </main>
     </div>
   );
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,6 +7,9 @@ function Hero(props) {
                 <div className="block">
                     <h1 className="h1">{props.heading}</h1>
                     <h2 className="h2">{props.title}</h2>
+                    <div className="hero__body">
+                        <p className="p p--medium">{props.body}</p>
+                    </div>
                 </div>
             </div>
         </section>
