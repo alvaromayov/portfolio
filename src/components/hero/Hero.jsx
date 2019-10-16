0107e5e0e3f5409cef7e9b6060facc1d1635d085
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-16 15:21:52 +0100

    Add hero animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,8 +5,10 @@ function Hero(props) {
         <section className="hero hero--dark">
             <div className="container">
                 <div className="block">
-                    <h1 className="h1">{props.heading}</h1>
-                    <h2 className="h2">{props.title}</h2>
+                    <div className="hero__heading">
+                        <h1 className="h1">{props.heading}</h1>
+                        <h2 className="h2">{props.title}</h2>
+                    </div>
                     <div className="hero__body">
                         <p className="p p--medium">{props.body}</p>
                     </div>
