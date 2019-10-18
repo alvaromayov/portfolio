532cfef1f10ff1220477b9a4826192d1e467ffad
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-18 12:01:06 +0100

    Add zenscroll and add functionality

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {ReactComponent as DownArrow} from '../../assets/icons/down-arrow.svg';
+import zenscroll from 'zenscroll';
 
-function Hero(props) {
-    return (
-        <section className="hero hero--dark">
-            <div className="container">
-                <div className="block">
-                    <div className="hero__heading">
-                        <h1 className="h1">{props.heading}</h1>
-                        <h2 className="h2">{props.title}</h2>
-                    </div>
-                    <div className="hero__body">
-                        <p className="p p--medium">{props.body}</p>
-                        <div className="hero__scroll-link">
-                            <span className="p p--small hero__link-text">Show me</span>
-                            <DownArrow className="hero__down-icon hero__down-icon--white" />
+class Hero extends Component {
+
+    scroll() {
+        zenscroll.setup(null, 60)
+        const nextSection = document.querySelector('#next-section');
+        zenscroll.to(nextSection)
+    }
+
+    render() {
+        return (
+            <section className="hero hero--dark">
+                <div className="container">
+                    <div className="block">
+                        <div className="hero__heading">
+                            <h1 className="h1">{this.props.heading}</h1>
+                            <h2 className="h2">{this.props.title}</h2>
+                        </div>
+                        <div className="hero__body">
+                            <p className="p p--medium">{this.props.body}</p>
+                            <div className="hero__scroll-link" onClick={this.scroll}>
+                                <span className="p p--small hero__link-text">Show me</span>
+                                <DownArrow className="hero__down-icon hero__down-icon--white" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
 
 export default Hero;
