9b7f7bca51c4936e717831cecf545cccb7addf42
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-22 12:34:59 +0100

    Updates

---
+++ precache-manifest.4a2314678688a762647524cc43346b03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0b7714ba1e2e1c5c18181196a59aeb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "812068e92d793198dc95",
+    "url": "/portfolio/static/css/main.43822eeb.chunk.css"
+  },
+  {
+    "revision": "9c23d24d7716123497ab",
+    "url": "/portfolio/static/js/2.0436d047.chunk.js"
+  },
+  {
+    "revision": "812068e92d793198dc95",
+    "url": "/portfolio/static/js/main.06daec03.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "00fd0c7b55c475e3c6a9d06fc71333b4",
+    "url": "/portfolio/static/media/AM-logo.00fd0c7b.svg"
+  },
+  {
+    "revision": "c0259fb2eb754fd23edd69a30087a13c",
+    "url": "/portfolio/static/media/down-arrow.c0259fb2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.66e07cfa8c234caab38362e4bc11974f.js"
+  "/portfolio/precache-manifest.4a2314678688a762647524cc43346b03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
