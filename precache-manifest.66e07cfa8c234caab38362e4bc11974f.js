a898ae2162f97b37271f826e1f654a647b00a329
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-22 12:15:31 +0100

    Updates

---
+++ precache-manifest.66e07cfa8c234caab38362e4bc11974f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96109b4551ecf476688e397dd976f00",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5e3b626d2b265f3c8b62",
+    "url": "/home/static/css/main.43822eeb.chunk.css"
+  },
+  {
+    "revision": "9c23d24d7716123497ab",
+    "url": "/home/static/js/2.0436d047.chunk.js"
+  },
+  {
+    "revision": "5e3b626d2b265f3c8b62",
+    "url": "/home/static/js/main.d79eb2bb.chunk.js"
+  },
+  {
+    "revision": "565fcc2182ffea41cd96",
+    "url": "/home/static/js/runtime-main.2d5b51f7.js"
+  },
+  {
+    "revision": "00fd0c7b55c475e3c6a9d06fc71333b4",
+    "url": "/home/static/media/AM-logo.00fd0c7b.svg"
+  },
+  {
+    "revision": "c0259fb2eb754fd23edd69a30087a13c",
+    "url": "/home/static/media/down-arrow.c0259fb2.svg"
+  }
+]);
