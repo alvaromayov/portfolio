49dae49e2a6d79543672bcd3c56ae6c55612fb26
Alvaro Mayo <alvaromayov@gmail.com>
2019-10-29 12:42:44 +0000

    Add swiper functionality and styles

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,26 @@
+import Swiper from 'react-id-swiper';
+
 import React from 'react';
 import Project from './Project';
 
 function Projects() {
+
+    const params = {
+        slidesPerView: 2,
+        breakpoints: {
+            1024: {
+                slidesPerView: 3
+            }
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+      }
 
     return (
         <section>
@@ -12,11 +31,15 @@ function Projects() {
                             <h2 className="projects__heading">Projects</h2>
                             <span className="projects__subheading">Check out some of the stuff I have built</span>
                         </div>
-                        <div className="projects__list">
-                            <Project title="Weather App" description="This is the app I have made to check the weather" image="12" />
-                            <Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" />
-                            <Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" />
-                        </div>
+                        <Swiper {...params}>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="Weather App" description="This is the app I have made to check the weather" image="https://openweather.co.uk/storage/app/media/we-are-pleased-announce-our-new-statistical-weather-data-api-now-available.png" /></div>
+                            <div><Project title="See all projects" description="Discover the rest of things I have worked on" image="" /></div>
+                        </Swiper>
                     </div>
                 </div>
             </div>
